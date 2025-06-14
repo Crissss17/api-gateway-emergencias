24240bbd6abd5060c74bdd578d7358a8bb6c0b12
Fabián Toledo <metraplox@gmail.com>
2025-06-14 11:16:31 -0400

    Agregar módulo principal

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { IncidentesModule } from './incidentes/incidentes.module';
+import { RecursosModule } from './recursos/recursos.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { WhatsappModule } from './whatsapp/whatsapp.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+      sortSchema: true,
+      playground: true,
+      introspection: true,
+      context: ({ req }) => ({ headers: req.headers }),
+      subscriptions: {
+        'graphql-ws': true,
+        'subscriptions-transport-ws': true,
+      },
+    }),
+    IncidentesModule,
+    RecursosModule,
+    DashboardModule,
+    WhatsappModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
